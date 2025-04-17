fdf7b5bd966885d2bc0a6044ee710b52deb514fb
goomba212 <leo.turato217@gmail.com>
2025-04-17 15:55:43 +0100

    Eighth Commit: Mums House

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+import time 
 
 pygame.init()
 
@@ -23,7 +25,7 @@ class Grid:
             "IOOOIOOOOOOOOOOOIOOI",
             "IOIIIOOIIIIIIIOOIOOI",
             "IOOOOOOOOOOOOOOOIOOI",
-            "IIIIIIIIIIIIIIIIaIIII"
+            "IIIIIIIIIIIIIIIIIIII"
         ]
 
     def draw(self):
@@ -41,11 +43,17 @@ class Pathfinder:
     def __init__(self, cellSize, cellmap):
         self.target = ()
         self.location = ()
+        
         self.cellSize = cellSize
         self.cellmap = cellmap
-        self.emptyCells = self.findEmptyCells()
-        self.junctions = self.getJunctions()
+        self.cellmapSize = len(cellmap[1])*len(cellmap)
+        
+        self.emptyCells : list = self.findEmptyCells()
+        self.junctions : list = self.getJunctions()
+        self.cellNumbers : list  = self.resetNumbers()
+        
         self.path = []
+        self.paths = []
 
     def cellEmpty(self, coord):
         return self.cellmap[coord[1]][coord[0]] != "I"
@@ -139,7 +147,8 @@ class Pathfinder:
         return encounterTypes, distances
 
     def createPaths(self):
-        self.path.clear()
+        self.paths.clear()
+        self.cellNumbers : list  = self.resetNumbers()
         checkList = [self.location]
         listPos = 0
         visited = set()
@@ -167,28 +176,62 @@ class Pathfinder:
                 elif i == 3:
                     end = (current[0], current[1] + distances[i])
 
-                self.path.append((current, end))
+                self.paths.append((current, end))
 
                 if encounterTypes[i] == 2:
                     targetFound = True
                     break
                 elif encounterTypes[i] == 0:
                     checkList.append(end)
+            
+            self.setNumberPath(self.getCellNumber(current), current, end)
+                    
+    def getCellNumber(self, coord):
+        return self.cellNumbers[coord[1]][coord[0]]
+    
+    def setCellNumber(self, coord, number):
+        if self.getCellNumber(coord) == 0:
+            self.cellNumbers[coord[1]][coord[0]] = number
+                
+    def setNumberPath(self, startingNumber, coord1 : tuple, coord2 : tuple):
+        additionNumber = 0
+        for y in range(coord1[1]+coord2[1]):
+            self.setCellNumber((coord1[0], coord1[1]-y), startingNumber+additionNumber)
+            additionNumber += 1
+            
+        additionNumber = 0
+        for x in range(coord1[0]+coord2[0]):
+            self.setCellNumber((coord1[0]-x, coord1[1]), startingNumber+additionNumber)
+            additionNumber += 1
+            
+    def resetNumbers(self):
+      numbers : list = []
+      for y in range(len(self.cellmap)):
+          row : int = []
+          for x in range(len(self.cellmap[y])):
+              if (x, y) not in self.emptyCells: row.append(-1)
+              else: row.append(0)
+          numbers.append(row)
+      return numbers
 
     def draw(self):
         cellSize = self.cellSize
         for junction in self.junctions:
             pygame.draw.rect(screen, (255, 100, 0),
                              (junction[0] * cellSize, junction[1] * cellSize, cellSize, cellSize))
-
+        
+        for y in range(len(self.cellNumbers)):
+            for x in range(len(self.cellNumbers[y])):
+                cellNumber = self.getCellNumber((x, y))
+                #pygame.draw.rect(screen, (int(1*cellNumber), int(1*cellNumber), int(1*cellNumber)), (self.target[0] * cellSize, self.target[1] * cellSize, cellSize, cellSize))
+        
         if self.target:
-            pygame.draw.rect(screen, (255, 0, 0),
-                             (self.target[0] * cellSize, self.target[1] * cellSize, cellSize, cellSize))
+            pygame.draw.rect(screen, (255, 0, 0), (self.target[0] * cellSize, self.target[1] * cellSize, cellSize, cellSize))
+            
         if self.location:
-            pygame.draw.rect(screen, (0, 0, 255),
-                             (self.location[0] * cellSize, self.location[1] * cellSize, cellSize, cellSize))
+            pygame.draw.rect(screen, (0, 0, 255), (self.location[0] * cellSize, self.location[1] * cellSize, cellSize, cellSize))
 
-        for start, end in self.path:
+        for start, end in self.paths:
             startPos = (start[0] * cellSize + cellSize / 2, start[1] * cellSize + cellSize / 2)
             endPos = (end[0] * cellSize + cellSize / 2, end[1] * cellSize + cellSize / 2)
             pygame.draw.line(screen, (0, 255, 0), startPos, endPos, 2)
@@ -219,8 +262,14 @@ while running:
         if not grid.cellTaken((gridX, gridY)):
             pathfinding.target = (gridX, gridY)
             pathfinding.createPaths()
-
+            
     screen.fill((0, 0, 0))
     grid.draw()
     pathfinding.draw()
     pygame.display.update()
+    print(pathfinding.cellNumbers[gridY][gridX])
+    time.sleep(0.2)
+  
+sys.exit()
+pygame.quit()
+    
